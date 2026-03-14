bdc0a1d024252e251b778a6d20dd0a6f9455a83b
bmonagan <bernardmonagan@gmail.com>
2026-03-13 20:14:59 -0400

    Fix import paths for home content in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./styles.css";
-import home_content from "./home";
-import menu_content from "./menu";
-import about_content from "./about";
+import home_content from "./home.js";
+// import menu_content from "./menu";
+// import about_content from "./about";
 
+const content = document.getElementById("content");
+content.appendChild(home_content);
+// content.appendChild(menu_content);
+// content.appendChild(about_content);
