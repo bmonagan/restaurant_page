32c50965012d10bc76ee7798ace0ecd9f603c7bc
bmonagan <bernardmonagan@gmail.com>
2026-03-13 20:27:22 -0400

    Import menu content and update button listener for menu navigation

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import "./styles.css";
 import home_content from "./home.js";
-// import menu_content from "./menu";
+import menu_content from "./menu.js";
 // import about_content from "./about";
 
 const content = document.getElementById("content");
 content.appendChild(home_content);
-// content.appendChild(menu_content);
 // content.appendChild(about_content);
 
 function clear_content() {
     while (content.firstChild) {
         content.removeChild(content.firstChild);
     }}
-const home_btn = document.getElementById("home-btn");
+const home_btn = document.getElementById("home-button");
 home_btn.addEventListener("click", () => {
     clear_content();
     content.appendChild(home_content);
 });
 
+const menu_btn = document.getElementById("menu-button");
+menu_btn.addEventListener("click", () => {
+    clear_content();
+    content.appendChild(menu_content);
+});
